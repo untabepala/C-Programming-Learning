77fe86ea5cc1230e3eac3d23db235149ff9b25e4
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-02-19 04:10:03 +0530

    Add files via upload

---
+++ rev 1.4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+float base ,area, height;
+printf("Enter Base:");
+scanf("%f",&base);
+printf("Enter Height:");
+scanf("%f",&height);
+area=0.5*height*base;
+printf("Area of Triangle is:%.2f",area);
+
+
+return 0;
+}
